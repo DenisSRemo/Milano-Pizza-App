56d8bafe6a97930734c7c4ec8ba5d1b8cf8724cb
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-01-11 13:40:44 +0000

    initial commit

---
+++ Pizzeria Milano App/Assets/script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AWSSDK { };
+public class script : MonoBehaviour
+
+
+
+
+
+ {
+"Statement": [{
+    "Effect": "Allow",
+    "Action": [
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Scan",
+        "dynamodb:UpdateItem"
+    ],
+    "Resource": "arn:aws:dynamodb:us-west-2:123456789012:table/MyTable"
+}]
+}
+    // Start is called before the first frame update
+   
