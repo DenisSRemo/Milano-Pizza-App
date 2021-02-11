bcf451256acd4dd90d93910967b10d89f0939a43
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-02-11 22:19:20 +0000

    commit

---
+++ Pizzeria Milano App/Assets/script.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon.DynamoDBv2.DataModel;
+
+
+
+
+
+namespace Assets.MenuAWSDynamoDB.Scripts
+{
+    [DynamoDBTable("Menu")]
+    public class Menu
+    {
+        [DynamoDBHashKey]   // Hash key.
+        public string FoodID { get; set; }
+
+        [DynamoDBProperty]
+        public int NumberOnMenu { get; set; }
+
+
+        [DynamoDBProperty]
+        public string Name { get; set; }
+
+
+        [DynamoDBProperty]
+        public string TypeFood { get; set; }
+
+        [DynamoDBProperty]
+        public float Price { get; set; }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
